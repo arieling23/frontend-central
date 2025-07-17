701705f00d31df401dce31e07042fb51539a03c1
ArielInguillay <arieling123@gmail.com>
2025-07-16 23:40:40 -0500

    feat(api): add 'api/preferences/reset' to the api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -64,9 +64,9 @@ const api = Object.assign(instance, {
     instance.post('/recovery/reset', { token, newPassword }),
 
   // ⚙️ Preferencias
-  getPreferences: () => instance.get('/preferences/me'),
-  updatePreferences: (data: Preferences) => instance.put('/preferences/me', data),
-  resetPreferences: () => instance.post('/preferences/reset'),
+  getPreferences: () => instance.get('api/preferences/me'),
+  updatePreferences: (data: Preferences) => instance.put('api/preferences/me', data),
+  resetPreferences: () => instance.post('api/preferences/reset'),
 
   // 🔐 RBAC
   getAllRoles: () => instance.get('/rbac/roles'),
